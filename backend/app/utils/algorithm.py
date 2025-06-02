264267f56e9e3d1406c8e0c95ae7d956093a5fe8
Wilmer Guzman <joelguzman2468@gmail.com>
2025-06-02 03:08:16 -0400

    Finished nearest neighbor implementation, and route optimization routes that accept csv files and exporting

--- backend/app/utils/algorithm.py
+++ backend/app/utils/algorithm.py
@@ -1,6 +1,6 @@
 # backend/app/utils/algorithm.py
 import networkx as nx
-from models.job import JobIn
+from models.job import JobIn, JobOut
 from math import radians, cos, sin, asin, sqrt, pi
 
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -63,6 +63,7 @@ def nearest_neighbor_optimized(jobs : list[JobIn], warehouse_location) -> list[J
                 shortest_distance = distance
                 nearest_job = job
 
+        r
         visited.append(nearest_job)
         unvisited.remove(nearest_job)
         current_lat = nearest_job.latitude
@@ -73,6 +74,35 @@ def nearest_neighbor_optimized(jobs : list[JobIn], warehouse_location) -> list[J
 
     
     # graph = build_job_graph(jobs)
-    return visited
+    return enrich_jobs(visited, warehouse_location[0], warehouse_location.longitude[1])
+
+def enrich_jobs(visited: list[JobIn], warehouse_lat, warehouse_lon, speed_kph=40.0) -> list[JobOut]:
+    enriched_jobs = []
+    total_distance = 0.0
+    total_time_minutes = 0.0
+
+    prev_lat = warehouse_lat
+    prev_lon = warehouse_lon
     
+    for index, job in enumerate(visited):
+        distance = haversine_distance(prev_lat, prev_lon, job.latitude, job.longitude)
+        time_minutes = (distance / speed_kph) * 60 if speed_kph > 0 else 0
+
+        total_distance += distance
+        total_time_minutes += time_minutes
+
+        enriched_job = JobOut(
+            **job.model_dump(),
+            route_position=index,
+            distance_from_prev_km=round(distance, 2),
+            cumulative_distancd_km=round(total_distance, 2),
+            eta_minutes=int(round(total_time_minutes))
+        )
+
+        enriched_jobs.append(enriched_job)
+
+        prev_lat = job.latitude
+        prev_lon = job.longitude
+
+    return enriched_jobs
     
